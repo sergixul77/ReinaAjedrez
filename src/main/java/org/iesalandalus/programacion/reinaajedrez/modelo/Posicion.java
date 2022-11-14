561767cd6f41d46ae51ed8d07e810cc2f6bc3a35
sergiogallegosguerrero <sergiogallegosguerrero@localhost>
2022-11-14 19:41:11 +0100

    He creado los metodos hascode para la clase Posicion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
 	
 	private int fila ;
@@ -67,6 +69,27 @@ public class Posicion {
 		
 		this.columna = columna;
 	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+	
+	
 	
 	
 	
