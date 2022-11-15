4b9058e978a9f90d0b35c621f7caf6f2fcdcf9bf
sergiogallegosguerrero <sergiogallegosguerrero@localhost>
2022-11-15 12:07:54 +0100

    He creado los metodos get y set con su respectiva comprobacion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -3,6 +3,33 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public class Reina {
 
 	private Color color;
-	
-	
+
+	private Posicion posicion;
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+
+		if (color == null) {
+			throw new IllegalArgumentException("El color que has puesto es nulo");
+		}
+
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+
+			throw new IllegalArgumentException("La posicion que has introducido es nula");
+
+		}
+		this.posicion = posicion;
+	}
+
 }
